616cfb152a319d19c5ab1858282c32e86888c3da
hyunchun <hyunchun@umich.edu>
2017-11-06 20:47:26 -0500

    added rest of files

---
+++ rpn.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+import operator
+from datetime import datetime
+from termcolor import colored
+import sys
+
+ops = {
+    '+': operator.add,
+    '-': operator.sub,
+}
+
+def calculate(myarg):
+    stack = list()
+
+
+    for token in myarg.split():
+        
+        try:
+            stack.append(int(token))
+
+        except ValueError:
+            
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            function = ops[token]
+            
+            result = function(arg1, arg2)
+            print(colored(result, 'red'))
+            stack.append(result)
+
+    return stack.pop()
+
+def main():
+    
+    while True:
+        input_text = input("rpn calc> ")
+        result = calculate(input_text)
+
+if __name__ == '__main__':
+    main()
